0af0680b6076a8f15e4b3dd37d1830883cd1510c
MS-gyq <134187087+MS-gyq@users.noreply.github.com>
2026-01-12 14:25:49 +0800

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,14 @@
+import random
+import torch
+import os
+import numpy as np
+
+
+def seed_everything(seed=666):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    np.random.seed(seed)
